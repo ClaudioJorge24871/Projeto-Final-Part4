59ed77ad7fff5c56da95fac79b8974a6df329250
owanedee <claudiojorge9090@gmail.com>
2025-01-20 22:13:12 +0100

    Alunos completo. Server ajustado.

---
+++ scriptAlunos.js
@@ -0,0 +1,24 @@
+function carregarAlunos(){
+    fetch('http://localhost:3000/aluno/alunos')
+        .then(response => response.json())
+        .then(dados => {
+            const listaAlunos = document.getElementById('lista_alunos');
+
+            listaAlunos.innerHTML = '';
+
+            dados.forEach(element => {
+                if(element.aluno){
+                    const li = document.createElement('li')
+                    const link = document.createElement('a')
+                    link.href = `fichaAluno.html?aluno=${element.aluno}`;
+                    link.textContent = element.aluno
+                    li.appendChild(link)
+                    listaAlunos.appendChild(li)
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao carregar dados:', error);
+        });
+}
+window.onload = carregarAlunos;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
 
 // Configuração da base de dados
 const db = mysql.createConnection({
@@ -22,5 +25,17 @@ db.connect((err) => {
             console.log('Servidor em execução na porta 3000');
         });
     }
+});
+
+app.get('/:atributo/:tabela', (req, res) => {
+    const atributo = req.params.atributo.split('_');
+    const tabela = req.params.tabela; // Extrai o nome da tabela da URL
+    console.log(tabela)
+    console.log(atributo)
+    // Consulta os dados da tabela dinâmica
+    db.query(`SELECT ?? FROM ??`, [atributo,tabela], (err, resultados) => {
+        if (err) return res.status(500).send(err);
+        res.json(resultados);  // Retorna os dados no formato JSON
+    });
 });
 
