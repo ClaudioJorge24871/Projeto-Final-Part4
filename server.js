f07cf11e1763c534b029f5e2084281340313fdd9
owanedee <claudiojorge9090@gmail.com>
2025-01-26 18:13:05 +0100

    correcao de ip como sempre

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors())
 
 // Configuração da base de dados
 const db = mysql.createConnection({
-    host: '192.168.0.104',
+    host: '192.168.0.103',
     user: 'claudio',
     password: 'Clauudio90#',
     database: 'compararplagiodb'
